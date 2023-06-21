7b23d44ae5c4c3b96db23d71a0af59d9d4c3a044
Sergio Jaramillo Luna <sergiojaramillo@mac-sergio.local>
2023-06-21 09:53:58 -0500

    solution ax1 introduction

---
+++ examples/intro-node-npm/server/index.js
@@ -0,0 +1,23 @@
+//const fs = require('fs')
+var colors = require('colors');
+
+// fs.writeFileSync('hello.txt', 'Hello from Node.js')
+
+console.log('Hello from Node.js'.inverse);
+
+
+// Al instalar node viene:
+// 1. npm
+// 2. node
+// 3. npx
+
+// Donde busca node los paquetes?
+// 1. core node
+// 2. node_modules
+// 3. archivos del proyecto
+
+
+// Como instalar un paquetes ?
+// 1. local -> npm install <package>
+// 2. global -> npm install -g <package>
+// 3. dev -> npm install --save-dev <package> ó npm install -D <package>
