a837711743e491741fd2ec860f1f6a2a48348d22
jesus bravo <jd_bravo24@hotmail.com>
2023-06-21 12:24:52 -0500

    indentation

--- nodeschool/javascripting/scope.js
+++ nodeschool/javascripting/scope.js
@@ -3,21 +3,21 @@ const b = 2;
 const c = 3;
 
 (function firstFunction() {
-  const b = 5;
-  const c = 6;
+    const b = 5;
+    const c = 6;
+    
+    (function secondFunction() {
+        const b = 8;
+        console.log(`a: ${a}, b: ${b}, c: ${c}`);
 
-  (function secondFunction() {
-    const b = 8;
-    console.log(`a: ${a}, b: ${b}, c: ${c}`);
+        (function thirdFunction() {
+            const a = 7;
+            const c = 9;
 
-    (function thirdFunction() {
-      const a = 7;
-      const c = 9;
-
-      (function fourthFunction() {
-        const a = 1;
-        const c = 8;
-      })();
+            (function fourthFunction() {
+                const a = 1;
+                const c = 8;
+            })();
+        })();
     })();
-  })();
 })();
