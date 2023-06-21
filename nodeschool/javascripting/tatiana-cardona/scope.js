36e8b4fd0bfb819f212e1cd7cfe8ffce4e5afdec
Tatiana Cardona <clleidytatina@gmail.com>
2023-06-21 14:38:27 -0500

    solution ax20 scope

--- nodeschool/javascripting/tatiana-cardona/scope.js
+++ nodeschool/javascripting/tatiana-cardona/scope.js
@@ -1,21 +1,19 @@
-const a = 1;
-let b = 2;
-let c = 3;
+const a = 1; const b = 2; const c = 3;
 
-(function firstFunction() {
-  c = 6;
+     (function firstFunction () {
+       const b = 5; const c = 6;
 
-  (function secondFunction() {
-    b = 8;
+       (function secondFunction () {
+         const b = 8;
+         console.log(`a: ${a}, b: ${b}, c: ${c}`);
+         (function thirdFunction () {
+           const a = 7; const c = 9;
 
-    (function thirdFunction() {
-      const a = 7;
-
-      (function fourthFunction() {
-        const a = 1;
-      })();
-    })();
-  })();
-})();
+           (function fourthFunction () {
+             const a = 1; const c = 8
+           })()
+         })()
+       })()
+     })()
 
 console.log(`a: ${a}, b: ${b}, c: ${c}`);
