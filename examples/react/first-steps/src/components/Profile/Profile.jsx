bcc078e3d2c04b5369f82004253a5396d191a104
Sergio Jaramillo Luna <sergiojaramillo@mac-sergio.local>
2023-06-27 10:02:59 -0500

    class sinthetic events

--- examples/react/first-steps/src/components/Profile/Profile.jsx
+++ examples/react/first-steps/src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import './Profile.css'
 const Profile = ({ name, age, image, background, children }) => {
 
   return (
-    <div className={`card ${background}`}>
+    <div className={`profileCard ${background}`}>
       <h2>{ name }</h2>
       <p>Edad: { age }</p>
       <img src={ image } />
---
+++ examples/react/first-steps/src/utils/eventHandlers.js
@@ -0,0 +1,3 @@
+export const handleClick = () => {
+  console.log('Click!')
+}
