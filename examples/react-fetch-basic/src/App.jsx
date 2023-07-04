bb0d02d4904fe9b1a564ed724cc9cf86785ace27
Sergio Jaramillo Luna <sergiojaramillo@mac-sergio.local>
2023-07-04 09:46:13 -0500

    class fetch react

--- examples/react-fetch-basic/src/App.jsx
+++ examples/react-fetch-basic/src/App.jsx
@@ -1,13 +1,73 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Form from './components/Form'
 import Item from './components/Item'
+import './App.css'
 
 function App() {
   const [user, setUser] = useState({})
   const [users, setUsers] = useState([])
+
+  const handleRead = () => {
+    const config = {
+      method: 'GET',
+    }
+    fetch('https://jsonplaceholder.typicode.com/users', config)
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.log(error))
+  }
+
+/*   const handleReadOne = () => {
+    const random = Math.floor(Math.random() * 10) + 1
+    fetch(`https://jsonplaceholder.typicode.com/users/${random}`)
+      .then((response) => response.json())
+      .then((data) => setUsers([...users, data]))
+      .catch((error) => console.log(error))
+  } */
+
+  // async - await 
+  const handleReadOne = async () => {
+    try {
+      const random = Math.floor(Math.random() * 10) + 1
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${random}`)
+      const data = await response.json()
+      setUsers([...users, data])
+
+    } catch (error) {
+      console.log('Entramos al catch')
+    }
+    
+  }
+
+
+  const handleCreate = async () => {
+    try {
+      const url = 'https://jsonplaceholder.typicode.com/users'
+      const fakeUser = {
+        name: 'Jhon Doe',
+        nickname: 'JhonDoee'
+      }
+      const config = {
+        method: 'POST',
+        body: JSON.stringify(fakeUser), // Javascript - json
+        headers: {
+          'Content-type': 'application/json',
+        }
+      }
+
+      
+      const response = await fetch(url, config)
+
+      const data = await response.json() // json - javascript
+      
+      alert('Usuario creado satisfactoriamente', data)
+
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <>
@@ -20,8 +80,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <button onClick={handleRead}>Leer</button>
+      <button onClick={handleReadOne}>Leer usuario</button>
+      <button onClick={handleCreate}>Crear</button>
       <div>
-      <Form user={user} />
         {users.map((user) => (
           <Item key={user.id} user={user} onSelectUser={setUser} />
         ))}
