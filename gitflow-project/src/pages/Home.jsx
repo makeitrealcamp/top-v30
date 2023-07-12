ff21742fc00eb337fc278fc054d224750df94a81
SergioJaramilloL <sergiojaramilloluna@gmail.com>
2023-07-12 15:18:29 -0500

    create page home

---
+++ gitflow-project/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Banner from "../components/banner"
+import Carousel from "../components/Carousel"
+
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home page</h1>
+      <Banner/>
+      <Carousel/>
+    </>
+  )
+}
+
+export default Home
