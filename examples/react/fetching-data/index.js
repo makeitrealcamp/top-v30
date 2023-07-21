d43979a3e6f72932bb727e8449132d83c5ec011c
Leonel Oliveros <leoneloliveros.co@gmail.com>
2023-07-21 10:38:45 -0500

    rick and morty

--- examples/react/fetching-data/index.js
+++ examples/react/fetching-data/index.js
@@ -1,11 +1,10 @@
-
+import fetch from 'node-fetch';
 
 async function getAllEpisodes() {
   const response = await fetch('https://rickandmortyapi.com/api/episode');
   const data = await response.json();
   return data.results;
 }
-
 
 async function getCharacterInfo(url) {
   const response = await fetch(url);
@@ -15,56 +14,48 @@ async function getCharacterInfo(url) {
 
 async function main() {
   const episodes = await getAllEpisodes();
-  // const charactersSet = new Set()
-
-  // episodes.forEach(episode => {
-  //   // 10 characters
-  //   episode.characters.slice(0, 10).forEach(characterUrl => {
-  //     charactersSet.add(characterUrl);
-  //   });
-  // });
-
-  // const promiseCharacters = [];
-  // for (const url of charactersSet) {
-  //   promiseCharacters.push(getCharacterInfo(url));
-  // }
-  // const characters = await Promise.all(promiseCharacters);
-
-  // const characters =  episodes.map((episode) => {
-  //   // 10 characters
-  //   return episode.characters.slice(0, 10);
+  // const characters = episodes.map(episode => {
+  //   return episode.characters.slice(0, 10)
   // })
 
 
-  // const characters = []
-  // episodes.forEach(item => {
-  //   characters.push(...item.characters.slice(0, 10))
-  // });
-  // const characterPromise = characters.map((url) => {
-  //   return getCharacterInfo(url);
-  // });
-  // const result = await Promise.all(characterPromise)
+  // otra forma 
+  // const episodes = episodes.reduce((acc, item)=> {
+  //   return [...acc, ...item.characters.slice(0, 10)]
+  // }, [])
 
-  const characters = episodes.reduce((acc, item) => {
-    return [...acc, ...item.characters.slice(0, 10)]
-  }, [])
+  // const charactersData = new Set()
+  // characters.forEach(character => {
+  //   charactersData.add(getCharacterInfo(character))
+  // });
 
-  const characterPromise = characters.map((url) => {
-    return getCharacterInfo(url);
+  // const result = await Promise.all(characters)
+
+  
+  const characters = new Set()
+  episodes.forEach(episode => {
+    episode.characters.slice(0, 10).forEach(character => {
+      characters.add(getCharacterInfo(character))
+    })
   });
-  const result = await Promise.all(characterPromise)
-
+  const result = await Promise.all(characters)
   const data = episodes.map((episode) => {
     return {
       title: `${episode.name} - ${episode.episode}`,
-      dateToAir: episode.air_date,
-      characters: episode.characters.slice(0, 10).map((url) => {
-        return result.find((item) => item.url === url)
+      air_date: `${episode.air_date}`,
+      characters: episode.characters.slice(0, 10).map((url)=> {
+        const { name, species } = result.find((item) => item.url === url)
+        return {
+          name, species
+        }
       })
     }
-  });
+  })
 
-  return data
+  console.log(data[0])
+
+
+  
 }
 
-main();
+main()
